118222004de29a580167ee9a446405d205470d3e
Indranil Sen <indranilsen010@gmail.com>
2017-01-27 13:08:26 -0500

    Added function to make api call

--- src/service.js
+++ src/service.js
@@ -1,8 +1,10 @@
+var http = require('http');
+
 var service = function() {
     var weatherApi = {
         endpoints: {
-            currentWeather: 'https://api.openweathermap.org/data/2.5/weather',
-            fiveDayForecast: 'https://api.openweathermap.org/data/2.5/forecast'
+            currentWeather: 'api.openweathermap.org/data/2.5/weather',
+            fiveDayForecast: 'api.openweathermap.org/data/2.5/forecast'
         },
         parameters: {
             location: '?q=',
@@ -11,7 +13,47 @@ var service = function() {
             coordsLongitude: '&lon='
         },
         apiKey: '&APPID='
-    }
+    };
+
+    var makeApiCall = function (url) {
+            var request = http.get(url, function(res) {
+                const statusCode = res.statusCode;
+                const contentType = res.headers['content-type'];
+                var error;
+                var rawData = '';
+
+                if (statusCode !== 200) {
+                    error = new Error('Error: ', statusCode);
+                } else if (contentType !== 'application/json') {
+                    error = new Error('Invalid content-type: ', contentType);
+                }
+
+                if (error) {
+                    console.log("=========>", error.message);
+                    res.resume();
+                    return;
+                }
+
+                res.setEncoding('utf8');
+
+                res.on('data', function (rawDataChunk) {
+                    rawData += rawDataChunk;
+                });
+
+                res.on('end', function() {
+                    try {
+                        var data = JSON.parse(rawData);
+                        console.log(data);
+                    } catch (exception ) {
+                        console.log(exception .message);
+                    }
+                });
+            });
+
+            request.on('error', function (err) {
+                console.log("=========>*", err);
+            });
+    };
 
     var getWeather = function(weatherType, inputType, data) {
         var url;
@@ -40,10 +82,8 @@ var service = function() {
             url += weatherApi.apiKey + process.env.KEY;
             console.log(url);
         }
-    };
 
-    var getFiveDayForecast = function (type, data) {
-        console.log(type, data);
+        makeApiCall('http://'+url);
     };
 
     return {
