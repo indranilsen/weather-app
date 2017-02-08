88188d0c71ce81f1163b38a340a1fc797322f3f5
Indranil Sen <indranilsen010@gmail.com>
2017-02-07 22:11:49 -0500

    Refactored architecture for a single server solution and increased modularity

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 var express = require('express');
+var path = require('path');
 var dotenv = require('dotenv');
+var compression = require('compression');
 
 var router = require('./src/routes');
 
@@ -14,6 +16,10 @@ app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
+
+app.use(compression());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', router);
 
