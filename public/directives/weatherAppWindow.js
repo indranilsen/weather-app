ec3568f3398696291c28a5b23a18ff7c35e6db1b
Indranil Sen <indranilsen010@gmail.com>
2017-02-11 18:26:29 -0500

    Tabs now switching properly

--- public/directives/weatherAppWindow.js
+++ public/directives/weatherAppWindow.js
@@ -4,10 +4,22 @@ app.directive('weatherAppWindow', function() {
         controller: function($scope, $rootScope) {
             $scope.page = 'currentWeather';
             $scope.showData = false;
+            $scope.showCurrentWeather = true;
+            $scope.showFiveDayForecast = false;
 
             $scope.$on('data-received', function() {
                 $scope.showData = true;
             });
+
+            $scope.showTab = function(tab) {
+                if(tab === 'currentWeather') {
+                    $scope.showCurrentWeather = true;
+                    $scope.showFiveDayForecast = false;
+                } else if (tab === 'fiveDayForecast') {
+                    $scope.showCurrentWeather = false;
+                    $scope.showFiveDayForecast = true;
+                }
+            };
         }
     };
 });
